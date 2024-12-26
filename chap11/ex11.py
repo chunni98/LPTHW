6130769dd4fa87241ac856d1fa2fca5b19c28ec9
shachiDebian <shachi1758@outlook.com>
2024-12-26 15:32:12 +0800

    完成 chap11
    close #2

---
+++ chap11/ex11.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.11
+# -*- coding: utf-8 -*-
+# \Datetime 2024-12-26 14:53
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end = ' ')
+height = input()
+#print("How much do you weight?", end = ' ')
+weight = input("How much do you weight? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+print(int(input("input a number: ")))
+print(int(3.6))
